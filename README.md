b46a951c6ecb86901af68550fcb079a30bdd73b3
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-03-06 19:08:13 -0700

    added assignment info screenshot and README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+expressServGooglePlay
+
+Thinkful Assignment
+Server-side programming with Node and Postgres module
+
+![Assignment requirements screenshot](/expressServGooglePlayAssignmentInfo.png)
--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('common'));
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+});
+
 const playStore = require('./playStore.js');
 app.get('/apps', (req, res) => {
     const { sort, genre } = req.query;
@@ -38,8 +43,4 @@ app.get('/apps', (req, res) => {
         })
     res
         .json(results)
-});
-    
-app.listen(8000, () => {
-    console.log('Server started on PORT 8000');
 });
