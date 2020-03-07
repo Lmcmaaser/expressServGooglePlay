8446f0401b129b70cd9825f58418480488d1fbf2
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-03-06 18:36:11 -0700

    debug .filter()

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(morgan('common'));
 const playStore = require('./playStore.js');
 app.get('/apps', (req, res) => {
     const { sort, genre } = req.query;
-    console.log(req.query, 'query')
+    console.log(req.query)
     
     // catches and returns error
     if (sort) {
@@ -18,13 +18,16 @@ app.get('/apps', (req, res) => {
     }
 
     let results = playStore
-    console.log(results)
-        .filter(app =>
-            app
+    console.log(results, 'results')
+        .filter(app =>{
+            console.log(app, 'app');
+            console.log(app.Genres.toLowerCase());
+            console.log(genre.toLowerCase());
+            return app
                 .Genres
                 .toLowerCase()
                 .includes(genre.toLowerCase())
-        )
+        })
     res
         .json(results)
 });
